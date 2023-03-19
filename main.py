d2a068d56c8f439d272233f9348d05ea3c0bdb4e
Rdroid-101 <raggav.subramani@gmail.com>
2023-03-19 12:49:24 +0530

    feat: retrieving data from every page

--- main.py
+++ main.py
@@ -5,23 +5,30 @@ import os
 
 load_dotenv()
 
-def fetch_data(url):
+f = open('repos.csv', "w+")
+f.close()
+flag = False
+
+def fetch_data(url, flag):
     file = 'repos.csv'
     columns = ['Owner ID', 'Owner Name', 'Owner Email', 'Repository ID', 'Repository Name', 'Status', 'Stars Count']
-
-    with open(file, 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=columns)
-        writer.writeheader()
-    
+    with open(file, 'a', newline='') as csvfile:
+          
         access_token = os.getenv('ACCESS_TOKEN')
         headers = {
             'Authorization': 'Token {}'.format(access_token)
         }
+
         resp = requests.get(url, headers=headers)
         repos = resp.json()
-
-        # repos = data['items']
-        print("\nSelected information about each repository:")
+        # print(repos)
+        if(len(repos) == 0):
+            flag = True
+            print("No more repositories to fetch")
+            return flag
+        print("\n adding to csv:")
+        writer = csv.DictWriter(csvfile, fieldnames=columns)
+        writer.writeheader()
         for repo in repos:
             try:
                 email = repo['owner']['email']
@@ -36,5 +43,10 @@ def fetch_data(url):
                 'Status': 'Private' if repo['private'] else 'Public',
                 'Stars Count': repo['stargazers_count']
             })
-
-fetch_data('https://api.github.com/user/repos')
+    return flag
+for i in range(1, 10):
+    if(flag == True):
+        break
+    else:
+        flag = fetch_data('https://api.github.com/user/repos?page={}&per_page=100'.format(i), flag)
+# fetch_data('https://api.github.com/user/repos?page=&per_page=100')
